99bd419c9a41d89450e256eb4ca25a209ca1a0e3
Luiz Otávio Puff <142064806+puffinho11@users.noreply.github.com>
2025-10-11 11:31:58 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require ('express');
+const client = require ('prom-client');
+
+const app = expres();
+const collectDefaultMetrcs = client.collectDefaulMetrics;
+collectDefaultMetrics;
+
+const counter = new client.Counter({
+    
+    name:"app_requests_total",
+    help:"contador de requisições recebidas"
+
+});
+
+app.get('/', (req,res)=>{
+    counter.inc();
+    res.send("Promethes+grafana+Kubernetes");
+});
+
+app.get('/metrics', async (req,res) =>{
+    res.set("content-type", client.register.contentType);
+    res.end(await client.register.metrics())
+});
+
+app.listen(300, () => {
+    console.log("App running on port 3000");
+});
