145a7945add807be661e31173ffa5da906b3ad7d
Luiz Otávio Puff <ens-luizpuff@ugv.edu.br>
2025-10-28 21:23:26 -0300

    28-10-2025

--- server.js
+++ server.js
@@ -1,27 +1,40 @@
-const express = require ('express');
-const client = require ('prom-client');
+const express = require('express')
+const client = require('prom-client')
 
-const app = expres();
-const collectDefaultMetrcs = client.collectDefaulMetrics;
-collectDefaultMetrics;
+const app = express()
 
-const counter = new client.Counter({
-    
-    name:"app_requests_total",
-    help:"contador de requisições recebidas"
+const collectDefaultMetrics = client.collectDefaultMetrics
+collectDefaultMetrics()
 
+const requestCounter = new client.Counter({
+  name: 'app_requests_total',
+  help: 'Contador de requisições recebidas pela aplicação'
+})
+
+const responseTimeHistogram = new client.Histogram({
+  name: 'app_response_time_seconds',
+  help: 'Histograma do tempo de resposta das requisições em segundos',
+  buckets: [0.1, 0.3, 0.5, 1, 2, 5]
+})
+
+app.use((req, res, next) => {
+  const end = responseTimeHistogram.startTimer()
+  res.on('finish', () => {
+    end()
+  })
+  next()
+})
+
+app.get('/', (req, res) => {
+  requestCounter.inc()
+  res.send('Aplicação com Prometheus + Grafana + NGINX + Kubernetes');
 });
 
-app.get('/', (req,res)=>{
-    counter.inc();
-    res.send("Promethes+grafana+Kubernetes");
-});
-
-app.get('/metrics', async (req,res) =>{
-    res.set("content-type", client.register.contentType);
-    res.end(await client.register.metrics())
-});
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType)
+  res.end(await client.register.metrics())
+})
 
 app.listen(3000, () => {
-    console.log("App running on port 3000");
-});
+  console.log('Aplicação rodando na porta 3000')
+})
